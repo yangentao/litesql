4b3b87af4d758a9e74a29df21b4d53b3d59c4fee
Entao <entaoyang@163.com>
2025-11-07 06:32:40 +0800

    readme

--- example/model_example.dart
+++ example/model_example.dart
@@ -47,6 +47,11 @@ void main() {
   println(ls);
   // [{"id":1,"name":"yang","add":"Peiking","age":99}]
 
+  From(Person).update([Person.name >> "entao"], where: Person.id.EQ(1));
+  // 2025-11-07 06:31:10.998 D SQL: UPDATE Person SET name = ? WHERE id = 1
+  From(Person).dump();
+  // 2025-11-07 06:31:10.998 D SQL: {id: 1, name: entao, add: Peiking, age: 99}
+
   p2.delete();
   // 2025-10-31 14:30:04.385 D xlog: DELETE FROM Person WHERE id = 1
   From(Person).dump();
